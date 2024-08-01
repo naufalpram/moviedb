59689fdac33e95721cf44628737d2b046829dd2c
Naufal Pramudya Yusuf <naufal.pramudya@sg-dsa.com>
2024-08-01 15:07:10 +0700

    feat: add more content to login approved page

--- src/pages/Auth/LoginApproved.jsx
+++ src/pages/Auth/LoginApproved.jsx
@@ -2,6 +2,12 @@ import { useEffect } from 'react'
 import { useAuth } from '../../hooks/useAuth'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
+const RedirectingMeme = () => (
+    <>
+    <div className='w-full h-0 pb-[75%] relative'><iframe src="https://giphy.com/embed/ddZ2mYhxhh69wEpSVz" width="100%" height="100%" frameBorder="0" className="giphy-embed absolute" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/DerekTee-url-binary-redirecting-ddZ2mYhxhh69wEpSVz">via GIPHY</a></p>
+    </>
+)
+
 const LoginApproved = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
@@ -17,7 +23,14 @@ const LoginApproved = () => {
   });
 
   return (
-    <div>Redirecting...</div>
+    <main className="w-[100vw] h-screen -mb-72 flex">
+       <section className="w-full h-1/2 flex justify-center">
+          <div className='flex flex-col gap-8'>
+            <RedirectingMeme />
+            <h2 className='font-medium text-2xl'>We are logging you in, please wait until you get redirected to home page</h2>
+          </div>
+       </section>
+    </main>
   )
 }
 
